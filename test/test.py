b11f36d7435fceb14de6c2cfda6e9891636e266b
Jiseok-Seo <103990167+JiseokSeo@users.noreply.github.com>
2023-03-21 01:20:27 +0900

    23-03-21

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ import torchvision
 
 # 설정 파라미터
 model_path = './models/best.pt'                # 모델 경로
-input_video = 'sample_video_data\sample1.mp4'  # 디텍션을 수행할 비디오 경로
+input_video = './sample_video_data/sample1.mp4'  # 디텍션을 수행할 비디오 경로
 output_video = './results/sample1_result.mp4'  # 디텍션 결과를 저장할 경로
 
 # 모델 임포팅
@@ -14,5 +14,5 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', model_path)
 
 #디텍션 수행, 결과는 output_video로 저장됩니다
 detection.detection(model = model,
-                     input_video = input_video,
-                     output_video = output_video)
+                    input_video = input_video,
+                    output_video = output_video)
