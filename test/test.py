440bdc2cf69cfce69ebf4e5c67a0e3c14b55697d
JiseokSeo <tjwltjr69@gmail.com>
2023-03-20 22:58:07 +0900

    23-03-20

--- test/test.py
+++ test/test.py
@@ -3,12 +3,10 @@ import torch
 import sys
 import os
 
-sys.path.append('./utils')
-
 # 설정 파라미터
-model_path = r'models\best.pt'               # 모델 경로
-input_video = 'data\sample1.mp4'             # 디텍션을 수행할 비디오 경로
-output_video = 'results\sample1_result'      # 디텍션 결과를 저장할 경로
+model_path = './models/best.pt'                # 모델 경로
+input_video = './data/sample1.mp4'             # 디텍션을 수행할 비디오 경로
+output_video = './results/sample1_result'      # 디텍션 결과를 저장할 경로
 
 # 모델 임포팅
 model = torch.hub.load('ultralytics/yolov5', 'custom', model_path)
